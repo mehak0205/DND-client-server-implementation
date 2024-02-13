9e5ee83f3d7c6d424fdfcce18c00d14e7cf9e282
Mehak Thakral <99324678+mehak0205@users.noreply.github.com>
2024-02-13 11:21:34 +0530

    Update processCommand.c

--- src/server/processCommand.c
+++ src/server/processCommand.c
@@ -1,1 +1,26 @@
 
+#include "../../hdr/server_header.h"
+
+void processCommand(int client_socket) {
+    int client_id;
+    char choice;
+    char num_groups[50]="NA";
+    char  global='-';
+    //Reading client info-----------
+    read(client_socket, &choice, sizeof(choice));
+    read(client_socket, &client_id, sizeof(client_id));
+    if(choice=='H')
+    {
+            handleIncomingCall(client_id);
+    }
+    else
+    {
+        if(choice=='A'){
+                read(client_socket, &global, sizeof(global));
+        }
+        if (choice == 'A' && global=='S'){
+                read(client_socket, &num_groups, sizeof(num_groups));
+        }
+        openFile(client_id,choice,global,num_groups);
+    }
+}
