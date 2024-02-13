da0e58a8c16c16cbb992b071999a7604dbafc3b6
Mehak Thakral <99324678+mehak0205@users.noreply.github.com>
2024-02-13 11:23:07 +0530

    Update warningToClient.c

--- src/server/warningToClient.c
+++ src/server/warningToClient.c
@@ -1,1 +1,15 @@
 
+#include "../../hdr/server_header.h"
+extern const char *log_levels[];
+
+void warningToClient(int c,int choice)
+{
+         if(choice=='A' && c=='A'){
+                                LOG(LOG_LEVEL_WARNING,"Already Activated,please deactivate first.\n");
+                                sendToClient(client_socket,"Already Activated,please deactivate first");
+         }
+         else if(choice=='D' && c=='D'){
+                                LOG(LOG_LEVEL_WARNING,"Already Deactivated,please activate first.\n");
+                                sendToClient(client_socket,"Already Deactivated,please Activate first");
+         }
+}
